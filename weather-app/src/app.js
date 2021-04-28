901e821c5725a4548286b4db3ca38175ada69b67
larisaturcu <turcu.larisa@gmail.com>
2021-04-28 16:37:45 +0200

    link the calls to geocode and weather

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -2,19 +2,19 @@ const weather = require('./utils/weather')
 const geocode = require('./utils/geocode')
 
 const location = 'Lyon';
-// weather(location, (error, {description} = {}) => {
-//   if (error) {
-// 		console.log('Something went wrong when calling weather: ' + error)
-// 	} else {
-// 		console.log('Weather in ' + location + ' is '+  description);
-// 	}
-// });
 
-
-geocode(location, (error, {latitude, longitude} = {}) => {
+geocode(location, (error, { latitude, longitude } = {}) => {
   if (error) {
-		console.log('Something went wrong when calling geocode: ' + error)
-	} else {
-		console.log('Location of ' + location + ' is: '+  latitude + ',' + longitude);
-	}
+    console.log('Something went wrong when calling geocode: ' + error)
+  } else {
+    console.log('Location of ' + location + ' is: ' + latitude + ',' + longitude);
+    weather(latitude, longitude, (error, { description } = {}) => {
+      if (error) {
+        console.log('Something went wrong when calling weather: ' + error)
+      } else {
+        console.log('Weather in ' + location + ' is ' + description);
+      }
+    });
+
+  }
 });
--- weather-app/src/utils/weather.js
+++ weather-app/src/utils/weather.js
@@ -1,23 +1,21 @@
 const request = require('postman-request');
 
-const weatherReq = (city, callback) => {
-console.log(city);
-    const url = 'http://api.weatherstack.com/current?access_key=9ae707a764a12655d4d3dfe12339e126&query='+city;
-    request({url, json:true}, 
-        (error,response, body) => {
-            console.log('error:', error); // Print the error if one occurred
-        if (error) {
-            callback('unable to connect to api', undefined);
-        } else if (!body.current) {
-                callback('unable to connect retrieve weather for location ' + city, undefined);
-        } else {
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            callback(undefined, {
-                description: body.current.weather_descriptions[0]
-            });
-       
+const weatherReq = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=9ae707a764a12655d4d3dfe12339e126&query=' + latitude + ',' + longitude;
+    request({ url, json: true },
+        (error, response, body) => {
+            if (error) {
+                callback('unable to connect to api', undefined);
+            } else if (!body.current) {
+                callback('unable to connect retrieve weather for location ' + latitude + ',' + longitude, undefined);
+            } else {
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                callback(undefined, {
+                    description: body.current.weather_descriptions[0]
+                });
+
+            }
         }
-    }
 
     );
 
