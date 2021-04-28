51b5ea4741cb799658656f2b19902b37acaddde5
larisaturcu <turcu.larisa@gmail.com>
2021-04-28 16:19:30 +0200

    move weather servide to utils and call it from main app

--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -1,11 +1,10 @@
-const request = require('postman-request');
+const weather = require('./utils/weather')
 
-const url = 'http://api.weatherstack.com/current?access_key=9ae707a764a12655d4d3dfe12339e126&query=Lyon';
-request(url, (error, body) => {
+const location = 'Lyon';
+weather(location, (error, {description} = {}) => {
   if (error) {
-		return console.log("Error: " + error);
-	} 
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-}
-);
+		console.log('Something went wrong when calling weather: ' + error)
+	} else {
+		console.log('Weather in ' + location + ' is '+  description);
+	}
+});
